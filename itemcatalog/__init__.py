0c7538bb3e71fad4cc8ae3075868861a440c75c0
kyapchung <kyapchung@gmail.com>
2019-05-16 14:43:35 -0600

    Made change to app.run host ip

--- itemcatalog/__init__.py
+++ itemcatalog/__init__.py
@@ -385,4 +385,4 @@ def gdisconnect():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='http://52.37.173.65/', port=80)
+    app.run(host='52.37.173.65', port=80)
